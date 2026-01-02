d7a5124f18c9dd0f4ae6334486a38067843b19d4
Dreadmare <aimanhakimi1000@gmail.com>
2026-01-02 16:18:29 +0800

    Implemented GUI components

--- src/oop_group_project/MainGUI.java
+++ src/oop_group_project/MainGUI.java
@@ -14,4 +14,37 @@ public class MainGUI extends JFrame {
 	private JTable historyTable;
 	private DefaultTableModel tableModel;
 	private DatabaseManager manager;
+	
+	public MainGUI() {
+		manager = new DatabaseManager();
+		setTitle("Projectile Trajectory Calculator");
+		setSize(500,400);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(new BorderLayout(10,10));
+		
+		JPanel inputPanel = new JPanel(new GridLayout(3,2,5,5));
+		inputPanel.add(new JLabel("Initial Velocity (m/s): "));
+		velocityField = new JTextField();
+		inputPanel.add(velocityField);
+		
+		calculateBtn = new JButton("Calculate");
+		clearBtn = new JButton("Clear");
+		inputPanel.add(calculateBtn);
+		inputPanel.add(clearBtn);
+		
+		String[] columns = {"Type","Velocity", "Angle", "Max Range (m)"};
+		tableModel = new DefaultTableModel(columns,0);
+		historyTable = new JTable(tableModel);
+		
+		saveBtn = new JButton("Save");
+		add(inputPanel,BorderLayout.NORTH);
+		add(new JScrollPane(historyTable),BorderLayout.CENTER);
+		add(saveBtn,BorderLayout.SOUTH);
+		
+		setupListeners();
+	}
+	
+	private void setupListeners() {
+		
+	}
 }
