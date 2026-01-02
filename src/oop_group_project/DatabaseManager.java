5fa23da11240f682cc1038ac58df1a974c37552a
Dreadmare <aimanhakimi1000@gmail.com>
2026-01-02 16:44:31 +0800

    Updated

--- src/oop_group_project/DatabaseManager.java
+++ src/oop_group_project/DatabaseManager.java
@@ -3,6 +3,9 @@ import java.util.*;
 import java.sql.*;
 
 public class DatabaseManager {
+	private final String url = "jdbc:mysql://localhost:3306/projectile_db";
+	private final String user = "root";
+	private final String password = "";
 	
 	private List<Projectile> historyList = new ArrayList<>();
 	public void addSimulation(Projectile p) {
@@ -11,7 +14,7 @@ public class DatabaseManager {
 	
 	public void saveToDatabase(Projectile p, double range) {
 		try {
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/projectile_db","root","");
+			Connection conn = DriverManager.getConnection(url, user, password);
 			String query = "INSERT INTO results (type, velocity, angle, max_range) VALUES (?,?,?,?,?)";
 			PreparedStatement pstmt = conn.prepareStatement(query);
 			pstmt.setString(1, "Standard Ball");
@@ -20,5 +23,19 @@ public class DatabaseManager {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void deleteSimulation(int id) throws SQLException {
+		String query = "DELETE FROM results WHERE id = ?";
+		try (Connection conn = DriverManager.getConnection(url, user, password); PreparedStatement pstmt = conn.prepareStatement(query)) {
+			pstmt.setInt(1, id);
+			pstmt.executeUpdate();
+		}
+	}
+	
+	public ResultSet fetchAllData() throws SQLException {
+		Connection conn = DriverManager.getConnection(url, user, password);
+		Statement stmt = conn.createStatement();
+		return stmt.executeQuery("SELECT * FROM results");
 	}
 }
